edc47bf7c56698959b92971494471f411227788a
jbotaldrich <jbot.aldrich@gmail.com>
2013-12-07 23:05:25 -0800

    Added a creature stats interface

--- PlanFinderLib/Data/ICreature.cs
+++ PlanFinderLib/Data/ICreature.cs
@@ -7,15 +7,20 @@ namespace PlanFinderLib
 
 		Guid ID { get; set;}
 		string Name{get; set}
+		int CR{get; set;}
+		CreatureType Type {get; set;}
+		string Alignment{ get; set;}
 		CreatureSize size{get; set;}
 		CreatureOrigin Origin {get; set;}
-		CreatureType Type {get; set;}
+		int Initiative{get;set;}
+		CreatureSenses Senses{get;set;}
 		string Keywords {get; set;}
 		int Level {get; set;}
 		CreatureOffense Offense{get; set;}
 		CreatureDefense Defense{get; set;}
 		CreatureStats Stats{get; set;}
-		
+		CreatureTactics Tactics{get; set;}
+
 
 	}
 }
